dbafcad602218cdbdd1b2ad034767ae7ee912786
Dishashet <shetdisha94@gmail.com>
2025-03-13 13:49:25 +0100

    deploy to cloudflare

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -3,14 +3,25 @@ import tailwindcss from '@tailwindcss/vite';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // extends: ['../ui'],
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', 'nitro-cloudflare-dev'],
   devtools: { enabled: true },
+
   css: ['~~/assets/css/main.css'],
 
   future: {
     compatibilityVersion: 4,
   },
+
   compatibilityDate: '2025-03-02',
+
+  nitro: {
+    preset: 'cloudflare-pages',
+
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
+    },
+  },
 
   vite: {
     plugins: [tailwindcss()],
